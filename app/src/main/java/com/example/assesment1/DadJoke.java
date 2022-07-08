15c3c83e525075a74b88bb0bfb6f064dc801fe2c
Mustafa <mustafaelfturi@gmail.com>
2022-07-08 02:30:30 -0400

    Initial commit

---
+++ app/src/main/java/com/example/assesment1/DadJoke.java
@@ -0,0 +1,83 @@
+package com.example.assesment1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class DadJoke extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dad_joke);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView  = findViewById(R.id.navigationMenu);
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.string_open, R.string.string_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (item.getItemId()){
+                    case R.id.id11:
+                        Intent i = new Intent(DadJoke.this,MainActivity.class);
+                        startActivity(i);
+                        break;
+                    case R.id.id22:
+                       // Toast.makeText(DadJoke.this, "Joke Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.id32:
+                        finishAffinity();
+
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_item, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.id1:
+                Toast.makeText(this, "You clicked on item 1", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.id2:
+                Toast.makeText(this, "You clicked on item 2", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+}
